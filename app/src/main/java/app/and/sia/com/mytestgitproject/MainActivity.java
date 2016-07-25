b129291eab3d29622a55503b21c307db6479760b
mzare747 <masoudzare747@gmail.com>
2016-07-25 17:12:31 +0330

    my Second commint _ commenting some parts of codes

--- app/src/main/java/app/and/sia/com/mytestgitproject/MainActivity.java
+++ app/src/main/java/app/and/sia/com/mytestgitproject/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        // Hello
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
